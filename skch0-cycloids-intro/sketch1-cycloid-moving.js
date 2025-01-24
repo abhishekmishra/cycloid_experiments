de91c784743028a1191a6563e0e4803c13468814
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-01-24 17:29:56 +0530

    make the wheel move across the canvas

--- skch0-cycloids-intro/sketch1-cycloid-moving.js
+++ skch0-cycloids-intro/sketch1-cycloid-moving.js
@@ -5,22 +5,33 @@ let cycloid_moving_sketch = function(p) {
     p.setup = function() {
         let canvas = p.createCanvas(200, 200);
         canvas.parent('sketch1-cycloid-moving');
-
-        let centerX = 100;
-        let centerY = 100;
-        let radius = 50;
-        theta = 0; // Initial theta in degrees
-
-        cycloidWheel = new CycloidWheel(p, centerX, centerY, radius, theta);
+        p.resetWheel();
     };
 
     p.draw = function() {
         p.background(220);
-        // Animate theta from 0 to 360 every 5 seconds
-        let delta = (p.millis() / 50000) * 360 % 360 - theta; 
+        
+        // calculate additional rotation based on time
+        // Convert milliseconds to seconds and then to degrees
+        let delta = p.deltaTime / 1000 * 360; 
 
+        // rotate and draw
         cycloidWheel.rotate(delta);
         cycloidWheel.draw();
+
+        // Reset position and rotation if the wheel goes past the canvas
+        if (cycloidWheel.centerX > p.width) {
+            p.resetWheel();
+        }
+    };
+
+    p.resetWheel = function() {
+        let centerX = 0;
+        let centerY = 100;
+        let radius = 25;
+        theta = 0; // Initial theta in degrees
+
+        cycloidWheel = new CycloidWheel(p, centerX, centerY, radius, theta);
     };
 };
 
